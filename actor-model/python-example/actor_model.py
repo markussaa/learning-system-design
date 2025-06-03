ad63e70e55234b9fad78f6463f745b206d6f0f36
Markus Saarijärvi <markus.saarijarvi@hotmail.com>
2025-06-03 07:24:37 +0200

    adds the actor base class

--- actor-model/python-example/actor_model.py
+++ actor-model/python-example/actor_model.py
@@ -1,8 +1,19 @@
 import asyncio
 
+class Actor:
+    def __init__(self):
+        pass
+    
+    async def send(self, message):
+        print(message)
+
+    async def run(self):
+        raise NotImplementedError()
+
 
 async def main():
-    print("Hello World")
+    actor = Actor()
 
+    await actor.send("Hello World")
 
 asyncio.run(main())
